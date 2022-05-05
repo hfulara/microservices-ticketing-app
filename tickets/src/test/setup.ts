88dbd6d829f361f0acca83772b3398bddaff2756
hfulara <himanshu.fulara@gmail.com>
2022-05-05 15:14:34 +0530

    Added ticket service along with unit test

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,54 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import request from "supertest";
+import jwt from "jsonwebtoken";
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: MongoMemoryServer;
+beforeAll(async () => {
+  process.env.JWT_KEY = "secret";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+});
+
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: "12345",
+    email: "test@test.com",
+  };
+
+  // Create a JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string that is the cookie with encoded data
+  return [`session=${base64}`];
+};
