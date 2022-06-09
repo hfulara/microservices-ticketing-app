ac4000873a675353a7e607baaa08f9c86cfb55b5
Himanshu Fulara <himanshu.fulara@gmail.com>
2022-06-09 14:22:33 +0530

    Cross service data replication

---
+++ orders/src/routes/new.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from 'express';
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@hftickets/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
+import { Order, OrderStatus } from '../models/order';
+
+const router = express.Router();
+const EXPIRATION_WINDOW_SECOND = 15 * 60;
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .notEmpty()
+      .withMessage('TicketId must be provided')
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId is not valid'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    const ticket = await Ticket.findById({ ticketId });
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await ticket.isReserved;
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECOND);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket: ticket,
+    });
+
+    await order.save();
+
+    res.status(201).send({ order });
+  }
+);
+
+export { router as newOrderRouter };
