b6c2e11df130551e85e4c3e10e4c0e2e4d11e290
Himanshu Fulara <himanshu.fulara@gmail.com>
2022-07-07 18:30:46 +0530

    Completed implementation of handling payments and unit testing of it.

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -8,11 +8,9 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
-
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
@@ -33,23 +31,21 @@ const start = async () => {
       console.log('NATS connection closed!');
       process.exit();
     });
-
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
+    new OrderCreatedListener(natsWrapper.client).listen()
+    new OrderCancelledListener(natsWrapper.client).listen()
 
     await mongoose.connect(process.env.MONGO_URI);
-
-    console.log('Connected to Mongodb');
+    console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
-};
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000 !!!');
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
 start();
